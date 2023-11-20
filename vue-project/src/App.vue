9b2a73e3bcc56bd66de4db16458d412c1c86f4fb
ZEQUANR <675897584@qq.com>
2023-11-20 20:12:12 +0800

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+const log = async () => {
+  const blob = new Blob([1, 2, 3])
+  window.console.log(await blob.arrayBuffer())
+
+  const file = new File([1, 2, 3], "123.text")
+  window.console.log(file)
+}
+
+const onFileChange = (event) => {
+  window.console.log(event)
+}
+</script>
+
+<template>
+  <!-- <header>hhhhh</header> -->
+
+  <!-- <main>kkkk</main> -->
+
+  <input type="file" :change="onFileChange" />
+</template>
+
+<style scoped></style>
