af212102afb581b52a19be75f6a7c9446b6a39dd
ZEQUANR <675897584@qq.com>
2023-11-29 23:47:12 +0800

    Fix: File

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,39 @@
-<script setup>
-const log = async () => {
-  const blob = new Blob([1, 2, 3])
-  window.console.log(await blob.arrayBuffer())
-
-  const file = new File([1, 2, 3], "123.text")
-  window.console.log(file)
-}
-
-const onFileChange = (event) => {
-  window.console.log(event)
-}
-</script>
-
 <template>
-  <!-- <header>hhhhh</header> -->
-
-  <!-- <main>kkkk</main> -->
-
-  <input type="file" :change="onFileChange" />
+  <div></div>
 </template>
 
-<style scoped></style>
+<script setup>
+const blob = new Blob(["Hello world"])
+
+const buffer = new ArrayBuffer(blob)
+
+window.console.log(blob)
+
+// const wrapBuffer = new Int8Array(buffer)
+
+// const Hello = [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
+
+// const buffer = new ArrayBuffer(Hello.length)
+
+// const wrapBuffer = new Int8Array(buffer)
+
+// for (let i = 0; i < Hello.length; i++) {
+//   wrapBuffer[i] = Hello[i]
+// }
+
+// const blob = new Blob([buffer])
+
+// const reader = new FileReader()
+
+// reader.readAsText(blob)
+
+// window.console.log(reader)
+</script>
+
+<style scoped>
+/* #cake {
+  width: 200px;
+  height: 200px;
+  border: 1px solid black;
+} */
+</style>
