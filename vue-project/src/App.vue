b86eb424f07679f03cb91a1bd45cc9fd4254ed95
ZEQUANR <675897584@qq.com>
2023-12-19 14:42:48 +0800

    Fix: variable promotion

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,36 +3,12 @@
 </template>
 
 <script setup>
-let objA = {
-  name: "Li Ming",
-  score: [80, 60, 90],
-  select: function () {
-    return this.name
-  },
-  age: undefined,
+var a = 1
+function foo(a) {
+  function a() {}
+  var a = 2
+  var b = a
+  console.log(a)
 }
-
-function deepCopy(obj) {
-  // 对于非对象和null值，直接返回
-  if (obj === null || typeof obj !== "object") return obj
-
-  // 根据obj的类型来决定新创建对象的类型
-  let copy = Array.isArray(obj) ? [] : {}
-
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      // 递归调用deepCopy处理每个属性
-      copy[key] = deepCopy(obj[key])
-    }
-  }
-
-  return copy
-}
-
-let objB = deepCopy(objA)
-
-objA.select = "888"
-
-window.console.log(objA) // { name: "Li Ming", score: [80, 60, 90], select: ƒ, age: undefined }
-window.console.log(objB) // { name: "Li Ming", score: [80, 60, 90] }
+foo(3)
 </script>
