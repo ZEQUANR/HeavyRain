feb66c6fe86be1dfd86b39bd9a018efeba857ba8
ZEQUANR <675897584@qq.com>
2024-01-09 23:38:37 +0800

    Fix: add node javascript

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,4 +2,20 @@
   <div></div>
 </template>
 
-<script setup></script>
+<script setup>
+var o = function () {
+  var obj = {
+    a: 1,
+    b: 2,
+  }
+  return {
+    get: function (k) {
+      return obj[k]
+    },
+  }
+}
+
+const fun = o()
+
+window.console.log(fun.get("a"))
+</script>
