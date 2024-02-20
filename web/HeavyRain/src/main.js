071f55f4124ce3153e66a2f4e7ba181c4b3d57e7
ZEQUANR <675897584@qq.com>
2024-02-20 21:10:17 +0800

    Fix: web add router

--- web/HeavyRain/src/main.js
+++ web/HeavyRain/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from "vue"
 import ArcoVue from "@arco-design/web-vue"
+import ArcoVueIcon from "@arco-design/web-vue/es/icon"
+import router from "./router"
 import App from "./App.vue"
 import "@arco-design/web-vue/dist/arco.css"
 import "@/api/interceptor"
-import "./assets/main.css"
+
+// import "./assets/main.css"
 
 const app = createApp(App)
 app.use(ArcoVue)
+app.use(ArcoVueIcon)
+app.use(router)
 app.mount("#app")
